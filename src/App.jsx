4d8c992bbb78dce4643b2169e8c8be26c14f9824
supardi_9512 <supardi@atomic.id>
2024-01-07 13:37:05 +0700

    deriving state across components

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
       <Log />
     </main>

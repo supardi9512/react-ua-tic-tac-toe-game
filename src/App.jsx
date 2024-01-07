293c548b4987a7167357873c70667404914a23fc
supardi_9512 <supardi@atomic.id>
2024-01-07 13:08:21 +0700

    prefer computed values and avoid unnecessary state management in game board and log

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,22 @@ function App() {
   const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "O" : "X");
-    setGameTurns();
+    setGameTurns(prevTurns => {
+      let currentPlayer = "X";
+
+      if(prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        {square: {row: rowIndex, col: colIndex}, player: currentPlayer}, 
+        ...prevTurns
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
